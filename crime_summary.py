630448b474d0e8f1f9e615011f9a54bc40e9c2ce
Krystle05 <50390584+Krystle05@users.noreply.github.com>
2019-05-07 22:22:56 -0500

    Add files via upload

---
+++ crime_summary.py
@@ -0,0 +1,15 @@
+
+
+
+with open("state_crime-CS152.csv", "r") as file:
+    print("State" + " "*12, "Reported Crimes")
+    next(file)
+    for aline in file:
+        values = aline.split(',')
+        state = values[10]
+        totalp = int(values[11])
+        totalv = int(values[15])
+        sum = (totalp + totalv)
+        mydict = dict([(state,sum)])
+        for (k,v) in mydict.items():
+            print("{:<21} {:>10,d}".format(k, v))
